6187ee324f80250f59b26e5827f41b62dc90d961
cliberman <chanaliberman1@gmail.com>
2021-01-03 20:08:06 -0600

    test throws null pointer exception

--- src/test/java/com/example/finalproject/controller/FinalControllerTest.java
+++ src/test/java/com/example/finalproject/controller/FinalControllerTest.java
@@ -3,6 +3,7 @@ package com.example.finalproject.controller;
 import com.example.finalproject.model.UserInfo;
 import com.example.finalproject.service.FinalService;
 import org.apache.catalina.User;
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
@@ -10,42 +11,50 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.MockitoRule;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import static org.assertj.core.api.Assertions.anyOf;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
-//@RunWith(MockitoJUnitRunner.class)
+//@ExtendWith(MockitoExtension.class)
+@RunWith(MockitoJUnitRunner.class)
 public class FinalControllerTest {
 
-    private FinalService finalService;
+    //private FinalService finalService;
 
     @Mock
-    private FinalService mockFinalService;
+    private FinalService finalServiceMock;
+
+//    @Rule
+//    public MockitoRule mockitoRule = MockitoJUnit.rule();;
 
     @InjectMocks
-    private FinalController mockFinalController;
+    private FinalController subject;
 
     @Test
     public void start_shouldCallService_andReturnItsResult() {
         UserInfo user = new UserInfo("Chana", 20);
-        when(mockFinalService.getNameAge(any(UserInfo.class))).thenReturn(new ResponseEntity<>("User is over bar mitzvah.", HttpStatus.OK));
-        Object actualResponseEntity = finalService.getNameAge(user);
-        assertThat(mockFinalService.getNameAge(user)).isEqualTo(new ResponseEntity<>("User is over bar mitzvah.", HttpStatus.OK));
-        verify(mockFinalService).getNameAge(user);
-    }
+        System.out.println("created the user");
+        when(finalServiceMock.getNameAge(any(UserInfo.class))).thenReturn(new ResponseEntity<>("User is over bar mitzvah.", HttpStatus.OK));
+        System.out.println("got past the when");
+        ResponseEntity actualResponseEntity = subject.getNameAge(user);
+        verify(finalServiceMock).getNameAge(user);
+        assertThat(actualResponseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(actualResponseEntity.getBody()).isEqualTo("User is over bar mitzvah.");
+        }
 
 
     @Test
     public void start_returnsUnauthorized_whenCalledWithNoName() {
         ResponseEntity expectedResponseEntity = new ResponseEntity("Sorry, you must enter a name.", HttpStatus.UNAUTHORIZED);
-        Object actualResponseEntity = finalService.getNameAge(new UserInfo("", 14));
+        Object actualResponseEntity = finalServiceMock.getNameAge(new UserInfo("", 14));
         assertThat(actualResponseEntity).isEqualTo(expectedResponseEntity);
     }
 
